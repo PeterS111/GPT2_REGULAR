82676f8468b3f959b93ebc6b5cc7244257aab14b
PeterS111 <44571558+PeterS111@users.noreply.github.com>
2020-07-09 12:16:32 +0100

    Delete run_g.py

--- run_g.py
+++
@@ -1,6 +0,0 @@
-import os
-
-nums = [1000, 2000, 3000, 4000] 
-
-for i in nums: 
-    os.system('python run_generation.py --model_type gpt2 --temperature 1.0 --top_k 50 --top_p 1.0 --model_name_or_path output/checkpoint-{} --length 300 --prompt "Immense light" --seed 68 --num_samples 1'.format(str(i))) 
